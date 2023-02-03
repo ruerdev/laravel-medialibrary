44276e8c3f3fb15f0635f3006659b4afdede962c
Rutger Broerze <contact@ruer.nl>
2023-02-03 14:46:44 +0100

    x

--- src/Support/ImageFactory.php
+++ src/Support/ImageFactory.php
@@ -3,11 +3,12 @@
 namespace Spatie\MediaLibrary\Support;
 
 use Spatie\Image\Image;
+use Spatie\ImageOptimizer\OptimizerChainFactory;
 
 class ImageFactory
 {
     public static function load(string $path): Image
     {
-        return Image::load($path)->useImageDriver(config('media-library.image_driver'));
+        return Image::load($path)->setOptimizeChain(OptimizerChainFactory::create()->setTimeout(180))->useImageDriver(config('media-library.image_driver'));
     }
 }
